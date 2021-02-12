5d6da64bfe625b61870637330896519f6f69c0b5
Jyde Kayo <jydetone@gmail.com>
2021-02-12 22:14:11 +0300

    register form updated

--- routes/home/user.js
+++ routes/home/user.js
@@ -51,77 +51,77 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
-router.get("/register", (req, res) => {
-  res.render("home/user/register",{title:"Register"});
-});
+// router.get("/register", (req, res) => {
+//   res.render("home/user/register",{title:"Register"});
+// });
 
-router.post("/register", (req, res) => {
-  let errors = [];
+// router.post("/register", (req, res) => {
+//   let errors = [];
 
-  if (!req.body.firstName) {
-    errors.push({ message: "please enter your first name" });
-  }
+//   if (!req.body.firstName) {
+//     errors.push({ message: "please enter your first name" });
+//   }
 
-  if (!req.body.lastName) {
-    errors.push({ message: "please add a last name" });
-  }
+//   if (!req.body.lastName) {
+//     errors.push({ message: "please add a last name" });
+//   }
 
-  if (!req.body.email) {
-    errors.push({ message: "please add an email" });
-  }
+//   if (!req.body.email) {
+//     errors.push({ message: "please add an email" });
+//   }
 
-  if (!req.body.password) {
-    errors.push({ message: "please enter a password" });
-  }
+//   if (!req.body.password) {
+//     errors.push({ message: "please enter a password" });
+//   }
 
-  if (!req.body.passwordConfirm) {
-    errors.push({ message: "This field cannot be blank" });
-  }
+//   if (!req.body.passwordConfirm) {
+//     errors.push({ message: "This field cannot be blank" });
+//   }
 
-  if (req.body.password !== req.body.passwordConfirm) {
-    errors.push({ message: "Password fields don't match" });
-  }
+//   if (req.body.password !== req.body.passwordConfirm) {
+//     errors.push({ message: "Password fields don't match" });
+//   }
 
-  if (errors.length > 0) {
-    res.render("home/user/register", {
-      errors: errors,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-    });
-  } else {
-    User.findOne({ email: req.body.email }).then((user) => {
-      if (!user) {
-        const newUser = new User({
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          email: req.body.email,
-          password: req.body.password,
-          admin: 0,
-        });
+//   if (errors.length > 0) {
+//     res.render("home/user/register", {
+//       errors: errors,
+//       firstName: req.body.firstName,
+//       lastName: req.body.lastName,
+//       email: req.body.email,
+//     });
+//   } else {
+//     User.findOne({ email: req.body.email }).then((user) => {
+//       if (!user) {
+//         const newUser = new User({
+//           firstName: req.body.firstName,
+//           lastName: req.body.lastName,
+//           email: req.body.email,
+//           password: req.body.password,
+//           admin: 0,
+//         });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
+//         bcrypt.genSalt(10, (err, salt) => {
+//           bcrypt.hash(newUser.password, salt, (err, hash) => {
+//             newUser.password = hash;
 
-            newUser.save().then((savedUser) => {
-              req.flash(
-                "success_message",
-                "You are now registered, please login"
-              );
+//             newUser.save().then((savedUser) => {
+//               req.flash(
+//                 "success_message",
+//                 "You are now registered, please login"
+//               );
 
-              res.redirect("/user/login");
-            });
-          });
-        });
-      } else {
-        req.flash("error_message", "That email exist please login");
+//               res.redirect("/user/login");
+//             });
+//           });
+//         });
+//       } else {
+//         req.flash("error_message", "That email exist please login");
 
-        res.redirect("/user/login");
-      }
-    });
-  }
-});
+//         res.redirect("/user/login");
+//       }
+//     });
+//   }
+// });
 
 router.get("/logout", (req, res) => {
   req.logOut();
