9f1544ee7694ada17a95cddc8de04a8c203d4613
Jyde Kayo <jydetone@gmail.com>
2021-02-21 01:20:51 +0300

    added font-family

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const upload = require("express-fileupload");
+// const dotenv = require("dotenv");
+// dotenv.config();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
@@ -23,13 +25,11 @@ const home = require("./routes/home/index");
 const user = require("./routes/home/user");
 // const db = require("./config/database");
 
-mongoose
-  .connect(process.env.MongoURI, {
+mongoose.connect(process.env.MongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
@@ -102,8 +102,8 @@ app.get("*", (req, res) => {
   });
 });
 
-const port = process.env.PORT 
+const port = process.env.PORT
 
 app.listen(port, () => {
-  console.log(`server started on port ${port} `);
+  console.log(`server started on port ${port}`);
 });
